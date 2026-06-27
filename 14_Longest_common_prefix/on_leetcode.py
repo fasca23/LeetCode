caef20a8a016996a21f306671cb52eef6f002c9f
Каменев Владимир <110154088+fasca23@users.noreply.github.com>
2026-06-27 07:26:34 +0300

    Update and rename on_leetres.py to on_leetcode.py

---
+++ 14_Longest_common_prefix/on_leetcode.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        if not strs:
+            return ""
+        
+        for i, ch in enumerate(strs[0]):
+            for s in strs[1:]:
+                if i >= len(s) or s[i] != ch:
+                    return strs[0][:i]
+        
+        return strs[0]
