a712a241f73539a614e3e15d4839446564703c0f
Каменев Владимир <110154088+fasca23@users.noreply.github.com>
2026-09-19 09:07:48 +0300

    Create on_leetcode.py

---
+++ 463_Island_Perimeter/on_leetcode.py
@@ -0,0 +1,21 @@
+class Solution:
+    def islandPerimeter(self, grid: list[list[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        perimeter = 0
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    # Каждая клетка даёт 4 ребра
+                    perimeter += 4
+                    
+                    # Верхний сосед — общий край, минус 2
+                    if i > 0 and grid[i-1][j] == 1:
+                        perimeter -= 2
+                    
+                    # Левый сосед — общий край, минус 2
+                    if j > 0 and grid[i][j-1] == 1:
+                        perimeter -= 2
+        
+        return perimeter
